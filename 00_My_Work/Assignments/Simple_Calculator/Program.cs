504a8fcb10178f650710d6d81478141ff0afc791
Paul Watts <paul.watts@me.com>
2024-03-10 12:22:57 +1100

    added my-work & assignments

---
+++ 00_My_Work/Assignments/Simple_Calculator/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.Write(@"Hello!
+Input the first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input the second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(@"What do you want to do with those numbers?
+[A]dd
+[S]ubtract
+[M]ultiply
+ ");
+char operation = Convert.ToChar(Console.ReadLine().ToUpper());
+Console.WriteLine();
+
+int result = 0;
+string operatorName = "";
+string errorFlag = "";
+
+switch (operation)
+{
+    case 'A':
+        result = number1 + number2;
+        operatorName = "+";
+        break;
+    case 'S':
+        result = number1 - number2;
+        operatorName = "-";
+        break;
+    case 'M':
+        result = number1 * number2;
+        operatorName = "*";
+        break;
+    default:
+        Console.WriteLine("Invalid choice!");
+        errorFlag = "error";
+        break;
+}
+if (errorFlag != "error")
+{
+    Console.WriteLine();
+    Console.WriteLine($"{number1} {operatorName} {number2} = {result}");
+
+}
+
+Console.WriteLine();
+Console.WriteLine("Press any key to close...");
+
+Console.ReadKey();
---
+++ 00_My_Work/My_Work/Section_01/Program.cs
@@ -0,0 +1,34 @@
+﻿//Console.WriteLine("Provide a number:");
+//int number = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("You provided: " + number);
+
+//Console.WriteLine("Hello!");
+//Console.WriteLine("What do you want to do?");
+//Console.WriteLine("[A]dd a TODO");
+//Console.WriteLine("[R]emove a TODO");
+//Console.WriteLine("[E]xit");
+//
+//string userChoice = Console.ReadLine();
+//Console.WriteLine("User input: " + userChoice);
+
+
+//var result = Add(10, 5);
+//Console.WriteLine("Result: " + result);
+string word;
+
+do
+{
+    Console.WriteLine("Enter a word longer than 10 letters:");
+    word = Console.ReadLine();
+    Console.WriteLine("You entered: " + word);
+} while (word.Length <= 10);
+
+
+Console.WriteLine("Thank you for playing!");
+Console.ReadKey();
+
+//int Add(int a, int b)
+//{
+//    return a + b;
+//}
