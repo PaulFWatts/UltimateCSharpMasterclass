83ddf6ab92fa0bb2e5f99be0ef4f6852cbd536f4
Paul Watts <paul.watts@me.com>
2024-03-11 17:42:06 +1100

    section 2 - in progress

---
+++ 00_My_Work/My_Work/Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿//int[] numbers = new int[5];
+
+//Console.WriteLine("Element 0: " + numbers[0]);
+//Console.WriteLine("Element 1: " + numbers[1]);
+//Console.WriteLine("Element 2: " + numbers[2]);
+//Console.WriteLine("Element 3: " + numbers[3]);
+//Console.WriteLine("Element 4: " + numbers[4]);
+//Console.WriteLine();
+
+var numbers = new [] { 2, 6, 1, 6, 19 }; // This is an array initializer and type is inferred
+
+var sum = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    sum += numbers[i];
+}
+Console.WriteLine("Sum of all elements: " + sum);
+
+//Console.WriteLine();
+//Console.WriteLine("Element 0: " + numbers[0]);
+//Console.WriteLine("Element 1: " + numbers[1]);
+//Console.WriteLine("Element 2: " + numbers[2]);
+//Console.WriteLine("Element 3: " + numbers[3]);
+//Console.WriteLine("Element 4: " + numbers[4]);
+//Console.WriteLine();
+
+//var firstFromEnd = numbers[^1]; // ^1 is the same as numbers[numbers.Length - 1]
+//var secondFromEnd = numbers[^2]; // ^2 is the same as numbers[numbers.Length - 2]
+//Console.WriteLine("First from end: " + firstFromEnd);
+//Console.WriteLine("Second from end: " + secondFromEnd);
+
+Console.ReadKey();
---
+++ 00_My_Work/My_Work/Sandbox/Program.cs
@@ -0,0 +1,19 @@
+﻿var totalCount = 0;
+
+for (int i = 0; i < 4; ++i)
+
+{
+    for(int j = 0; j < 3; ++j)
+    {
+        for(int k = 0; k < 5; ++k)
+        {
+            totalCount++;
+            Console.WriteLine($"i is {i}, j is {j}, k is {k}");
+        }   
+    }
+}   
+Console.WriteLine($"Total count is {totalCount}");
+Console.WriteLine("The loop is finished");
+
+
+Console.ReadKey();
