141a951eb44297568e8e1c682a1db417338fe70d
Paul Watts <paul.watts@me.com>
2024-03-12 12:16:28 +1100

    section 2 continued + folder reorganisation

---
+++ 00_My_Work/Section 02 - C# Fundamentals/My_Work/TryParse_method/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+bool isParsingSuccessfu1;
+do
+{
+    Console.WriteLine("Enter a number:");
+    var userInput = Console.ReadLine();
+    isParsingSuccessfu1 = int.TryParse(userInput, out var number);
+    if (isParsingSuccessfu1)
+    {
+        Console.WriteLine($"You entered: {number}");
+    }
+    else
+    {
+        Console.WriteLine("Invalid input. Please try again:");
+    }
+} while (!isParsingSuccessfu1);
+
+Console.ReadKey();
---
+++ 00_My_Work/Section 02 - C# Fundamentals/My_Work/out_keyword/Program.cs
@@ -0,0 +1,39 @@
+﻿var numbers = new[] { 10, -8, 2, 12, -17 };
+int countOfNonPositive;
+var onlyPositive = GetOnlyPositive(numbers, out countOfNonPositive);
+foreach (var number in onlyPositive)
+{
+    Console.WriteLine(number);
+}
+Console.WriteLine($"Count of non-positive numbers: {countOfNonPositive}");
+Console.WriteLine();
+Console.WriteLine("Press any key to exit...");
+
+
+
+Console.ReadKey();
+
+
+List<int> GetOnlyPositive(int[] numbers, out int countOfNonPositive) // out keyword
+{
+    var result = new List<int>();
+    countOfNonPositive = 0;
+    foreach (var number in numbers)
+    {
+        if (number > 0)
+        {
+            result.Add(number);
+        }
+        else
+        {
+            countOfNonPositive++;
+        }
+    }
+    return result;
+}
+
+
+
+
+
+
