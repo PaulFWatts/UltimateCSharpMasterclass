44cc7ff8925ba150b134f75212b127dce840c986
Paul Watts <paul.watts@me.com>
2024-03-24 14:27:28 +1100

    start Section 3 Basics of OOP

---
+++ 00_My_Work/Section 03 - Basics of OOP/My_Work/Understanding OOP with DateTime/Program.cs
@@ -0,0 +1,22 @@
+﻿
+var internationalPizzaDay23 = new DateTime(2023, 2, 9);
+
+Console.WriteLine("year is " + internationalPizzaDay23.Year);
+Console.WriteLine("month is " + internationalPizzaDay23.Month);
+Console.WriteLine("day is " + internationalPizzaDay23.Day);
+Console.WriteLine("day of week is " + internationalPizzaDay23.DayOfWeek);
+Console.WriteLine(internationalPizzaDay23.ToString("yyyy MMM dd"));
+
+var internationalPizzaDay24 = internationalPizzaDay23.AddYears(1);
+
+Console.WriteLine();
+Console.WriteLine("year is " + internationalPizzaDay24.Year);
+Console.WriteLine("month is " + internationalPizzaDay24.Month);
+Console.WriteLine("day is " + internationalPizzaDay24.Day);
+Console.WriteLine("day of week is " + internationalPizzaDay24.DayOfWeek);
+Console.WriteLine(internationalPizzaDay24.ToString("yyyy MMM dd"));
+
+
+
+Console.ReadKey();
+
