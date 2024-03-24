f3c050121ed3ea473e19a61ad4a07f9d8eab4c73
Paul Watts <paul.watts@me.com>
2024-03-24 19:19:49 +1100

    complete Our first class

---
+++ 00_My_Work/Section 03 - Basics of OOP/My_Work/Our first class/Program.cs
@@ -0,0 +1,36 @@
+﻿var rectangle1 = new Rectangle(5, 10);   
+
+Console.WriteLine("Width is " + rectangle1.Width);
+Console.WriteLine("Height is " + rectangle1.Height);
+Console.WriteLine("Circumference is " + rectangle1.CalculateCircumference());
+Console.WriteLine("Area is " + rectangle1.CalculateArea());
+Console.WriteLine();
+
+var rectangle2 = new Rectangle(2, 3);
+Console.WriteLine("Width is " + rectangle2.Width);
+Console.WriteLine("Height is " + rectangle2.Height);
+Console.WriteLine("Circumference is " + rectangle2.CalculateCircumference());
+Console.WriteLine("Area is " + rectangle2.CalculateArea());
+Console.ReadKey();
+
+class Rectangle
+{
+    public int Width;
+    public int Height;
+
+    public Rectangle(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public int CalculateCircumference()
+    {
+        return 2 * (Width + Height);
+    }
+
+    public int CalculateArea()
+    {
+        return Width * Height;
+    }
+}
